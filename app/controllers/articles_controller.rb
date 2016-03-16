e993cf6df407bafdb65735f6ce9de972cd026a89
Tushar Garg <tushar.garg@daffodilsw.com>
2016-03-16 19:21:14 +0530

    extract redundency and add delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
    @articles = Article.all
   end
@@ -19,24 +19,31 @@ class ArticlesController < ApplicationController
   end   
   
   def show 
-    @article = Article.find(params[:id])
+  
   end
   
   def edit
-     @article = Article.find(params[:id])
   end
 
   def update
-     @article = Article.find(params[:id])
      if @article.update(article_params)
-     	 flash[:notice] = "Article was successfully updated"
-     	 redirect_to article_path(@article)
-      else
-      render 'edit'
-   end  
-
+     	  flash[:notice] = "Article was successfully updated"
+     	  redirect_to article_path(@article)
+       else
+        render 'edit'
+     end   
+  end
+  
+  def destroy
+   
+   @article.destroy
+   flash[:notice] = "Article was successfully deleted"
+   redirect_to articles_path
   end
   private
+  def find_article_by_id
+  @article = Article.find(params[:id])
+  end
   def article_params 
    params.require(:article).permit(:title, :description)
   end
