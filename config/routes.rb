743345fa7863599b6b1b63eab295e753e7a98e2d
Tushar Garg <tushar.garg@daffodilsw.com>
2016-03-17 22:36:09 +0530

    Add User create and Edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,18 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
     root 'pages#home'
-
+    resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
            get 'about', to: 'pages#about'
+           get 'signup', to: 'users#new'
+          resources :users, except: [:new]
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-      resources :articles
+      
   # Example resource route with options:
   #   resources :products do
   #     member do
