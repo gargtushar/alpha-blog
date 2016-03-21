15c0377ae67e5a06f8bd11df33b042aec2f0161e
Tushar Garg <tushar.garg@daffodilsw.com>
2016-03-21 15:29:02 +0530

    Add user, edit user, login, logout and put some restrictions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
   before_action :find_article_by_id, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
-   @articles = Article.all
+   @articles = Article.paginate(page: params[:page], per_page: 1)
   end
 
   def new
@@ -10,7 +12,7 @@ class ArticlesController < ApplicationController
   
   def create
    @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
    	 flash[:Success] = "Article was successfully created"
      redirect_to article_path(@article)
@@ -47,5 +49,11 @@ class ArticlesController < ApplicationController
   end
   def article_params 
    params.require(:article).permit(:title, :description)
+  end
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own articles"
+      redirect_to root_path
+    end 
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 def new
 	@user = User.new
 end
 
+def index
+  @users = User.paginate(page: params[:page], per_page: 3)
+end
+
 def edit
-	@user = User.find(params[:id])
+
+end
+
+def show 
+ @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
 end
 
 def update
-	@user = User.find(params[:id])
 	 if @user.update(user_params)
 	 	flash[:success] = "Changes Saved Successfully #{@user.username}"
 	 	redirect_to articles_path
@@ -27,8 +36,19 @@ def create
   end
 end  
 private 
-def user_params
-  params.require(:user).permit(:username, :email, :password)
-end
 
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+  @user = User.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "Ypu can only edit your own account"
+      redirect_to root_path
+    end
+  end
 end
